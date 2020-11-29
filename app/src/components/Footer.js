85d15a02c98e202580d057effe3893da1adae2ef
Alex Stout <vickalexandra3@gmail.com>
2020-11-29 09:42:01 -0800

    commit

---
+++ app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h2>Copyright Alexandra Stout</h2>
+    </div>
+  );
+};
+
+export default Footer;
